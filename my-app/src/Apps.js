52eb56be4ee3038f9a64caa5a96331b4d63a77f4
ramesh02001 <rameshsharp2001@gmail.com>
2024-10-23 13:23:12 +0530

    df

---
+++ my-app/src/Apps.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import App from './App'; // Ensure this path is correct based on your project structure
+
+// const Apps = () => {
+//   return (
+//     <App />  // Renders the main App component
+//   );
+// }
+
+// export default Apps;
