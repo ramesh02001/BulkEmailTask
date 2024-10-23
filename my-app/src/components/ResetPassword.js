8ee7b024e3cdc0b0814b6c1959c5ca7c78586689
ramesh02001 <137371718+ramesh02001@users.noreply.github.com>
2024-10-23 19:58:29 +0530

    Update ResetPassword.js

--- my-app/src/components/ResetPassword.js
+++ my-app/src/components/ResetPassword.js
@@ -16,7 +16,7 @@ const ResetPassword = () => {
         setError(''); // Reset error before making a new request
 
         try {
-            const response = await fetch(`https://bulkemailtaskbackend-1.onrender.com/api/auth/reset-password/${token}`, {
+            const response = await fetch(`https://https://bulkemail2001.onrender.com/api/auth/reset-password/${token}`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
